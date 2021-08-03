74a3f0e6cb4d80932c8afe960d3b81ba93da6690
Miguel Betzay Dominguez Diaz <al221910421@gmail.com>
2021-08-03 12:06:27 -0500

    Mejora de horarios

--- resources/views/templates/agendarCita_quiropractica.blade.php
+++ resources/views/templates/agendarCita_quiropractica.blade.php
@@ -362,38 +362,140 @@
             <option value="4">4</option>
             <option value="5">5</option>
             <option value="6">6</option>
+        </select><br><br>
+        @if($nomdia != "Sabado")
+        <label>Hora*:</label><select id="hora" name="hora" required>
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="08:00">08:00</option>
+            <option value="09:00">09:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="12:00">12:00</option>
+            <option value="13:00">13:00</option>
+            <option value="14:00">14:00</option>
+            <option value="15:00">15:00</option>
+            <option value="16:00">16:00</option>
+        </select><br><br>
+        @elseif($nomdia == "Sabado")
+        <label>Hora*:</label><select id="hora" name="hora" required>
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="08:00">08:00</option>
+            <option value="09:00">09:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="12:00">12:00</option>
+            <option value="13:00">13:00</option>
+        </select><br><br>
+        @endif
+        @elseif($area == "Ultrasonido")   
+        <label>Consultorio*:</label><select id="consultorio" name="consultorio">
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="1">1</option>
+        </select><br><br>
+        <label>Hora*:</label><select id="hora" name="hora" required>
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="10:00">10:00</option>
+            <option value="10:30">10:30</option>
+            <option value="11:00">11:00</option>
+            <option value="11:30">11:30</option>
+        </select><br><br>   
+        @elseif($area == "Rayos x")
+        <label>Consultorio*:</label><select id="consultorio" name="consultorio">
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="1">1</option>
+        </select><br><br>
+        <label>Hora*:</label><select id="hora" name="hora" required>
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="08:30">08:30</option>
+            <option value="09:00">09:00</option>
+            <option value="09:30">09:30</option>
+            <option value="10:00">10:00</option>
+            <option value="10:30">10:30</option>
+            <option value="11:00">11:00</option>
+            <option value="11:30">11:30</option>
+            <option value="12:00">12:00</option>
+            <option value="12:30">12:30</option>
+            <option value="13:00">13:00</option>
+            <option value="13:30">13:30</option>
+            <option value="14:00">14:00</option>
+            <option value="14:30">14:30</option>
+            <option value="15:00">15:00</option>
+            <option value="15:30">15:30</option>
+            <option value="16:00">16:00</option>
+            <option value="16:30">16:30</option>
+        </select><br><br>
+        @elseif($area == "Rehabilitación")
+        <label>Consultorio*:</label><select id="consultorio" name="consultorio">
+            <option selected disabled value="">Elije una opción</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
             <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+        </select><br><br>
+        @if($nomdia != "Sabado")
+        <label>Hora*:</label><select id="hora" name="hora" required>
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="08:00">08:00</option>
+            <option value="09:00">09:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="12:00">12:00</option>
+            <option value="13:00">13:00</option>
+            <option value="14:00">14:00</option>
+            <option value="15:00">15:00</option>
+            <option value="16:00">16:00</option>
+        </select><br><br>
+        @elseif($nomdia == "Sabado")
+        <label>Hora*:</label><select id="hora" name="hora" required>
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="08:00">08:00</option>
+            <option value="09:00">09:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="12:00">12:00</option>
+            <option value="13:00">13:00</option>
+        </select><br><br>
+        @endif
+        @elseif($area == "Laboratorio")
+        <label>Consultorio*:</label><select id="consultorio" name="consultorio">
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="1">1</option>
+        </select><br><br>
+        <label>Hora*:</label><select id="hora" name="hora" required>
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="08:00">08:00</option>
+            <option value="08:15">08:15</option>
+            <option value="08:30">08:30</option>
+            <option value="08:45">08:45</option>
+            <option value="09:00">09:00</option>
+            <option value="09:15">09:15</option>
+            <option value="09:30">09:30</option>
+            <option value="09:45">09:45</option>
+            <option value="10:00">10:00</option>
+        </select><br><br>
+        @elseif($area == "Cámara hiperbárica")
+        <label>Consultorio*:</label><select id="consultorio" name="consultorio">
+            <option selected disabled value="">Elije una opción por favor</option>
+            <option value="1">1</option>
         </select><br><br>
         <label>Hora*:</label><select id="hora" name="hora" required>
             <option selected disabled value="">Elije una opción por favor</option>
             <option value="08:30">08:30</option>
             <option value="09:30">09:30</option>
-            <option value="10:30">10:30</option>
-            <option value="11:30">11:30</option>
-            <option value="12:30">12:30</option>
+            <option value="10:10">10:10</option>
+            <option value="11:10">11:10</option>
+            <option value="11:50">11:50</option>
+            <option value="12:50">12:50</option>
             <option value="13:30">13:30</option>
             <option value="14:30">14:30</option>
-            <option value="15:30">15:30</option>
-            <option value="16:00">16:00</option>
+            <option value="15:10">15:10</option>
         </select><br><br>
-        @elseif($area == "Imagenología")   
-        <label>Consultorio*:</label><select id="consultorio" name="consultorio">
-            <option selected disabled value="">Elije una opción</option>
-            <option value="1">Rayos X</option>
-            <option value="2">Ultrasonido</option>
-        </select><br><br>
-        <label>Hora*:</label><select id="hora" name="hora" required>
-            <option selected disabled value="">Elije una opción por favor</option>
-            <option value="08:30">08:00</option>
-            <option value="09:30">09:00</option>
-            <option value="10:30">10:00</option>
-            <option value="11:30">11:00</option>
-            <option value="12:30">12:00</option>
-            <option value="13:30">13:00</option>
-            <option value="14:30">14:00</option>
-            <option value="15:30">15:00</option>
-            <option value="16:00">16:00</option>
-        </select><br><br>          
         @endif
         @endif
         <input type="text" value="{{$curp}}" hidden name="curp" readonly >
